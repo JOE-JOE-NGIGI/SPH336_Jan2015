3e56305fce80bff6fe4200fed64d1a4065436d10
Joe Ngigi <liljosephtim@yahoo.com>
2015-03-10 19:56:11 +0300

    LED blinker added to convert integer to pass values that toggle the LEDs on or off

---
+++ out/main.d
@@ -0,0 +1,71 @@
+out/main.o: main.c gpio.h Mk60.h generics.h uart.h MK60DZ10.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h \
+ led_blinker.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdio.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/newlib.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stddef.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdarg.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/lock.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/stdio.h
+
+gpio.h:
+
+Mk60.h:
+
+generics.h:
+
+uart.h:
+
+MK60DZ10.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h:
+
+led_blinker.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdio.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/_ansi.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/newlib.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/config.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/ieeefp.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stddef.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdarg.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/reent.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/_ansi.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/lock.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/stdio.h:
---
+++ out/uart.d
@@ -0,0 +1,17 @@
+out/uart.o: uart.c uart.h MK60DZ10.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h
+
+uart.h:
+
+MK60DZ10.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h:
